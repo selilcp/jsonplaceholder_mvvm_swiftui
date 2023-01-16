daf8d887b2cdf61de1d626aff364563e1091d08f
selil <selilmcp@gmail.com>
2023-01-16 10:59:33 +0000

    Change tab view name

---
+++ Brighthr/View/MainTabView.swift
@@ -0,0 +1,27 @@
+//
+//  MainTabView.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+
+    var body: some View {
+        TabView{
+            PostListingCell()
+                .tabItem {
+                    Label("Editor", systemImage: "pencil.circle")
+
+                }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
