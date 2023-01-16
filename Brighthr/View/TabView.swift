f1c4c63561a23aaf7bf0eccc1c2b9e51b8d337a1
selil <selilmcp@gmail.com>
2023-01-16 10:41:36 +0000

    Create tab view

---
+++ Brighthr/View/TabView.swift
@@ -0,0 +1,17 @@
+//
+//  TabView.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import SwiftUI
+
+struct TabView: View {
+
+    var body: some View {
+        TabView{
+            
+        }
+    }
+}
