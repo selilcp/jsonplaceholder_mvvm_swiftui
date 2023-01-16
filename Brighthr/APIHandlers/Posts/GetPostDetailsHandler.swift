39ad5a2f47640373f37aa46189c20ed7946bc7f8
selil <selilmcp@gmail.com>
2023-01-16 17:51:49 +0000

    create handler for getting post details

---
+++ Brighthr/APIHandlers/Posts/GetPostDetailsHandler.swift
@@ -0,0 +1,32 @@
+//
+//  GetPostDetailsHandler.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import Foundation
+
+class GetPostDetailsHandler: APIHandler {
+    
+    func makeRequest(urlParam urls: [String: String]?,
+                     queryParam query: [String : String]?,
+                     bodyParam body: [String : Any]?) -> URLRequest? {
+        
+        var urlString =  APIPath().getPostDetails
+        if let urlParam = urls{
+            updateURL(url: &urlString, replacemetValues: urlParam)
+        }
+        if let url = URL(string: urlString) {
+            var urlRequest = URLRequest(url: url)
+            setDefaultHeaders(request: &urlRequest)
+            urlRequest.httpMethod = HTTPMethod.get.rawValue
+            return urlRequest
+        }
+        return nil
+    }
+    
+    func parseResponse(data: Data, response: HTTPURLResponse) throws -> Post {
+        return try defaultParseResponse(data: data, response: response)
+    }
+}
