cf6509b8687442db16590303fe9bdeac9c23d693
selil <selilmcp@gmail.com>
2023-01-16 17:49:31 +0000

    Create a handler for getting posts

---
+++ Brighthr/APIHandlers/Posts/GetPostsHandler.swift
@@ -0,0 +1,32 @@
+//
+//  GetPostsHandler.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import Foundation
+
+class GetPostsHandler: APIHandler {
+    
+    func makeRequest(urlParam urls: [String: String]?,
+                     queryParam query: [String : String]?,
+                     bodyParam body: [String : Any]?) -> URLRequest? {
+        
+        var urlString =  APIPath().getPost
+        if let urlParam = urls{
+            updateURL(url: &urlString, replacemetValues: urlParam)
+        }
+        if let url = URL(string: urlString) {
+            var urlRequest = URLRequest(url: url)
+            setDefaultHeaders(request: &urlRequest)
+            urlRequest.httpMethod = HTTPMethod.get.rawValue
+            return urlRequest
+        }
+        return nil
+    }
+    
+    func parseResponse(data: Data, response: HTTPURLResponse) throws -> Posts {
+        return try defaultParseResponse(data: data, response: response)
+    }
+}
