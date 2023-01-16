cc9aee9f64ef77322189eb8a09694458288b5c13
selil <selilmcp@gmail.com>
2023-01-16 10:44:59 +0000

    Create posts listing view

---
+++ Brighthr/View/PostListing.swift
@@ -0,0 +1,15 @@
+//
+//  PostListing.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import SwiftUI
+
+struct PostListing: View {
+    
+    var body: some View {
+        
+    }
+}
