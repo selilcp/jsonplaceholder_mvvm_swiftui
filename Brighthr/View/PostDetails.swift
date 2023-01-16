74e7b30d447b8d9c9b8ffee33b9692c2a3867963
selil <selilmcp@gmail.com>
2023-01-16 11:01:26 +0000

    Create post details view

---
+++ Brighthr/View/PostDetails.swift
@@ -0,0 +1,14 @@
+//
+//  PostDetails.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import SwiftUI
+
+struct PostDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
