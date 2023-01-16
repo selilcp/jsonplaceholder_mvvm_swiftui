13ca18494864722fde219060f89005363188d3d4
selil <selilmcp@gmail.com>
2023-01-16 11:17:43 +0000

    Design post detail view

--- Brighthr/View/PostDetails.swift
+++ Brighthr/View/PostDetails.swift
@@ -9,6 +9,34 @@ import SwiftUI
 
 struct PostDetails: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading,spacing: 6) {
+            Text("Title")
+                .bold()
+                .font(.title3)
+                .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 40))
+            Text("sub..")
+                .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 40))
+            Spacer()
+            
+            HStack(spacing: 40) {
+                Spacer()
+                Button("Saved") {
+                    
+                }
+                .foregroundColor(.black)
+                .frame(width: 120, height: 40)
+                .background(.orange)
+                .cornerRadius(8)
+                .disabled(true)
+            
+                NavigationLink("Comments", destination: CommentListing())
+                .foregroundColor(.black)
+                .frame(width: 120, height: 40)
+                .background(.green)
+                .cornerRadius(8)
+                Spacer()
+            }
+            Spacer()
+        }
     }
 }
