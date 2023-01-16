eecc6aeeeac461277e3dc63c773166d532c47a48
selil <selil.cp@mindster.in>
2023-01-16 10:29:56 +0000

    Create project

---
+++ Brighthr/BrighthrApp.swift
@@ -0,0 +1,20 @@
+//
+//  BrighthrApp.swift
+//  Brighthr
+//
+//  Created by selil on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BrighthrApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
